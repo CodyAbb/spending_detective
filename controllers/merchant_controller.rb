1cc4439d14d4609f77e619e92bba72b06f7632c9
Cody Abbott <cody.abbott16@gmail.com>
2019-12-15 10:54:04 +0000

    added merchant page controller

---
+++ controllers/merchant_controller.rb
@@ -0,0 +1,13 @@
+require('sinatra')
+require( 'sinatra/contrib/all' )
+require_relative( '../models/merchant.rb' )
+also_reload( '../models/*' )
+
+get ('/merchants/?') do
+  @merchants = Merchant.all
+  erb(:'merchants/index')
+end
+
+get ('/merchants/:id') do
+  @merchant = Merchant.find(params['id'].to_i)
+end
