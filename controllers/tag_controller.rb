9e46ecb99078096b77793792d745c26f5b93cb9d
Cody Abbott <cody.abbott16@gmail.com>
2019-12-16 11:22:10 +0000

    updating tags for full CRUD

--- controllers/tag_controller.rb
+++ controllers/tag_controller.rb
@@ -12,3 +12,30 @@ get ('/tags/:id') do
   @tag = Tag.find(params['id'].to_i)
   erb(:"tags/show")
 end
+
+# create
+post ('/tags') do
+  @tag = Tag.new(params)
+  @tag.save
+  erb(:create)
+end
+
+# edit
+get '/tags/:id/edit' do
+  @houses = House.all()
+  @tag = Tag.find(params[:id])
+  erb(:edit)
+end
+
+# update
+post '/tags/:id' do
+  student = Tag.new(params).update()
+  redirect to '/tags'
+end
+
+# destroy
+post '/tags/:id/delete' do
+  tag = Tag.find(params[:id])
+  tag.delete()
+  redirect to '/Tags'
+end
