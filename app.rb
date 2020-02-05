565c548ecc011dc469cb97c272e63e5c5b15c2d5
Cody Abbott <cody.abbott16@gmail.com>
2020-02-05 20:05:43 +0000

    removed unecessary class

--- app.rb
+++ app.rb
@@ -4,11 +4,6 @@ require_relative('controllers/tag_controller')
 require_relative('controllers/merchant_controller')
 require_relative('controllers/transaction_controller')
 
-class App < Sinatra::Base
-  use MerchantsController
-  use TagsController
-  use TransactionsController
-  get ('/?') do
-    erb(:'/index')
-  end
+get ('/?') do
+  erb(:'/index')
 end
