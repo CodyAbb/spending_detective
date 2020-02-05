8e73133e1ed5488ee54fc4da42b4f545ecb23c10
Cody Abbott <cody.abbott16@gmail.com>
2020-02-05 19:46:04 +0000

    database changes

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,7 @@ class SqlRunner
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'spending_tracker', host: 'localhost' })
+      db = PG.connect({ dbname: 'dd545br02ssm7k', host: 'ec2-3-213-192-58.compute-1.amazonaws.com', port: 5432, user: 'xhfwgpjtynxlgh', password: 'e403e46a9cffd5e544201af5f01b4cb52da6d39775d23e59a643dad6b546621b' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
